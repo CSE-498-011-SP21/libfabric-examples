945fd7e33bdccaff705da82a84ee4cee056651ae
dePaul Miller <dsm220@lehigh.edu>
2021-02-18 18:45:08 -0500

    Clean raii for fabrics.

--- wrappers/include/Fabric.hh
+++ wrappers/include/Fabric.hh
@@ -5,19 +5,144 @@
 #include <rdma/fabric.h>
 #include <rdma/fi_domain.h>
 #include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <atomic>
+#include <memory>
 
 #ifndef NETWORKLAYER_FABRICCXX_HH
 #define NETWORKLAYER_FABRICCXX_HH
 
+#define ERRCHK(x) error_check((x), __FILE__, __LINE__);
+
+inline void error_check(int err, std::string file, int line) {
+    if (err) {
+        std::cerr << "ERROR (" << err << "): " << fi_strerror(-err) << std::endl;
+        std::cerr << file << ":" << line << std::endl;
+
+        exit(1);
+    }
+}
+
 const int FIVersion = FI_VERSION(FI_MAJOR_VERSION, FI_MINOR_VERSION);
 
-struct EventQueue {
+class FabricInfo {
+public:
+    FabricInfo() : ref(new std::atomic_uint(1)) {
+        info = fi_allocinfo();
+    }
 
-    EventQueue(fid_fabric *fabric, fi_eq_attr *attr) {
-        if (fi_eq_open(fabric, attr, &eq, NULL)) {
+    FabricInfo(uint32_t version, const char *node, const char *service, uint64_t flags,
+               FabricInfo &hints) : ref(new std::atomic_uint(1)) {
+        ERRCHK(fi_getinfo(version, node, service, flags, hints.get(), &info));
+    }
+
+    FabricInfo(const FabricInfo &other) {
+        other.ref->operator++();
+        info = other.info;
+        ref = other.ref;
+    }
+
+    FabricInfo(FabricInfo &&other) {
+        info = other.info;
+        ref = other.ref;
+        other.ref = nullptr;
+        other.info = nullptr;
+    }
+
+    ~FabricInfo() {
+        if (info && ref->operator--() == 0) {
+            fi_freeinfo(info);
+            delete ref;
+        }
+    }
+
+    FabricInfo& operator=(const FabricInfo &other) {
+        if(&other == this)
+            return *this;
+        other.ref->operator++();
+        info = other.info;
+        ref = other.ref;
+        return *this;
+    }
+
+    FabricInfo& operator=(FabricInfo &&other) noexcept {
+        if(&other == this)
+            return *this;
+        info = other.info;
+        ref = other.ref;
+        other.ref = nullptr;
+        other.info = nullptr;
+        return *this;
+    }
+
+
+    fi_info *operator->() const {
+        return info;
+    }
+
+    fi_info *get() const {
+        return info;
+    }
+
+private:
+    fi_info *info;
+    std::atomic_uint *ref;
+};
+
+class Fabric {
+public:
+    Fabric(FabricInfo &info) : info_(info), ref(new std::atomic_uint(1)) {
+        ERRCHK(fi_fabric(info->fabric_attr, &fabric, nullptr));
+    }
+
+    Fabric(const Fabric &other) {
+        other.ref->operator++();
+        ref = other.ref;
+        fabric = other.fabric;
+        info_ = other.info_;
+    }
+
+    Fabric(Fabric &&other) {
+        fabric = other.fabric;
+        ref = other.ref;
+        other.ref = nullptr;
+        other.fabric = nullptr;
+        info_ = std::move(other.info_);
+    }
+
+    ~Fabric() {
+        if (fabric && ref->operator--() == 0) {
+            ERRCHK(fi_close(&fabric->fid));
+            delete ref;
+        }
+    }
+
+    fid_fabric *operator->() const {
+        return fabric;
+    }
+
+    fid_fabric *get() const {
+        return fabric;
+    }
+
+private:
+    fid_fabric *fabric;
+    std::atomic_uint *ref;
+    FabricInfo info_;
+};
+
+class EventQueue {
+public:
+    EventQueue(Fabric &fabric, fi_eq_attr *attr) : fabric_(fabric) {
+        if (fi_eq_open(fabric.get(), attr, &eq, nullptr)) {
             perror("Opening event queue:");
         }
     }
+
+    EventQueue(const EventQueue &) = delete;
+
+    EventQueue(EventQueue &&) = delete;
 
     ~EventQueue() {
         if (fi_close(&eq->fid)) {
@@ -25,30 +150,75 @@ struct EventQueue {
         }
     }
 
+    fid_eq *operator->() const {
+        return eq;
+    }
+
+    fid_eq *get() const {
+        return eq;
+    }
+
+private:
+    Fabric fabric_;
     fid_eq *eq;
 };
 
-struct AccessDomain {
-    AccessDomain(fid_fabric *fabric, fi_info *info) {
-        if (fi_domain(fabric, info, &domain, NULL))
+class AccessDomain {
+public:
+    AccessDomain(Fabric &fabric, FabricInfo &info) : ref(new std::atomic_uint(1)), info_(info), fabric_(fabric) {
+        if (fi_domain(fabric.get(), info.get(), &domain, nullptr))
             perror("Creating access domain:");
     }
 
+    AccessDomain(AccessDomain &&other) : info_(std::move(other.info_)), fabric_(std::move(other.fabric_)) {
+        domain = other.domain;
+        ref = other.ref;
+        other.ref = nullptr;
+        other.domain = nullptr;
+    }
+
+    AccessDomain(const AccessDomain & other) : info_(other.info_), fabric_(other.fabric_) {
+        other.ref->operator++();
+        domain = other.domain;
+        ref = other.ref;
+        info_ = other.info_;
+    }
+
     ~AccessDomain() {
+        if (domain && ref->operator--() == 0) {
             if (fi_close(&domain->fid)) {
                 perror("Closing access domain:");
             }
+            delete ref;
+        }
     }
 
+    fid_domain *operator->() const {
+        return domain;
+    }
+
+    fid_domain *get() const {
+        return domain;
+    }
+
+private:
     fid_domain *domain;
+    std::atomic_uint *ref;
+    FabricInfo info_;
+    Fabric fabric_;
 };
 
-struct CompletionQueue {
-    CompletionQueue(AccessDomain &d, fi_cq_attr *attr) {
-        if (fi_cq_open(d.domain, attr, &cq, NULL)) {
+class CompletionQueue {
+public:
+    CompletionQueue(AccessDomain &domain, fi_cq_attr *attr) : domain_(domain) {
+        if (fi_cq_open(domain_.get(), attr, &cq, nullptr)) {
             perror("Completion queue open:");
         }
     }
+
+    CompletionQueue(const CompletionQueue &) = delete;
+
+    CompletionQueue(CompletionQueue &&) = delete;
 
     ~CompletionQueue() {
         if (fi_close(&cq->fid)) {
@@ -56,23 +226,50 @@ struct CompletionQueue {
         }
     }
 
+    fid_cq *operator->() const {
+        return cq;
+    }
+
+    fid_cq *get() const {
+        return cq;
+    }
+
+private:
+
     fid_cq *cq;
+    AccessDomain domain_;
 };
 
-struct MemoryRegion {
-    MemoryRegion(AccessDomain &d, const void *buf, size_t len, uint64_t access, uint64_t offset, uint64_t requested_key,
-                 uint64_t flags) {
-        if (fi_mr_reg(d.domain, buf, len, access, offset, requested_key, flags, &mr, NULL)) {
+class MemoryRegion {
+public:
+    MemoryRegion(AccessDomain &domain, const void *buf, size_t len, uint64_t access, uint64_t offset,
+                 uint64_t requested_key,
+                 uint64_t flags) : domain_(domain) {
+        if (fi_mr_reg(domain_.get(), buf, len, access, offset, requested_key, flags, &mr, NULL)) {
             perror("");
         }
     }
+
+    MemoryRegion(const MemoryRegion &) = delete;
+
+    MemoryRegion(MemoryRegion &&) = delete;
 
     ~MemoryRegion() {
         if (fi_close(&mr->fid))
             perror("");
     }
 
+    fid_mr *operator->() const {
+        return mr;
+    }
+
+    fid_mr *get() const {
+        return mr;
+    }
+private:
+
     fid_mr *mr;
+    AccessDomain domain_;
 };
 
 
--- wrappers/src/test.cc
+++ wrappers/src/test.cc
@@ -9,11 +9,8 @@
 #include <rdma/fi_cm.h>
 
 int main(int argc, char **argv) {
-    auto hints = fi_allocinfo();
-    if (!hints) {
-        perror("fi_allocinfo");
-        return -1;
-    }
+
+    FabricInfo hints;
 
     hints->addr_format = FI_SOCKADDR_IN; // want addresses we are used to
     hints->ep_attr->type = FI_EP_MSG;
@@ -21,20 +18,9 @@ int main(int argc, char **argv) {
     hints->caps = FI_MSG | FI_RMA; // want RMA and MSG
     hints->mode = FI_CONTEXT | FI_LOCAL_MR | FI_RX_CQ_DATA;
 
-    fi_info *info;
+    FabricInfo info(FIVersion, nullptr, "8080", 0, hints);
 
-    if (fi_getinfo(FIVersion, NULL, "8080", 0, hints, &info)) {
-        perror("fi_getinfo");
-        return -1;
-    }
-
-    fi_freeinfo(hints);
-
-    fid_fabric *fabric;
-    if (fi_fabric(info->fabric_attr, &fabric, NULL)) {
-        perror("fi_fabric");
-        return -1;
-    }
+    Fabric fabric(info);
 
     fi_eq_attr eq_attr = {
             .size = 0,
@@ -44,9 +30,9 @@ int main(int argc, char **argv) {
             .wait_set = NULL,
     };
 
-    EventQueue *eq = new EventQueue(fabric, &eq_attr);
+    EventQueue eq(fabric, &eq_attr);
 
-    AccessDomain *domain = new AccessDomain(fabric, info);
+    AccessDomain domain(fabric, info);
 
     struct fi_cq_attr cq_attr = {
             .size = 0,
@@ -58,43 +44,34 @@ int main(int argc, char **argv) {
             .wait_set = NULL,
     };
 
-    CompletionQueue *cq = new CompletionQueue(*domain, &cq_attr);
+    CompletionQueue cq(domain, &cq_attr);
 
     char *buf = new char[256];
 
-    MemoryRegion *mr = new MemoryRegion(*domain, buf, 256, FI_REMOTE_READ | FI_REMOTE_WRITE | FI_SEND | FI_RECV,
-                                        0, 0, 0);
+    MemoryRegion mr(domain, buf, 256, FI_REMOTE_READ | FI_REMOTE_WRITE | FI_SEND | FI_RECV, 0,
+                                             0, 0);
 
-    std::cerr << fi_mr_key(mr->mr) << std::endl;
+    std::cerr << fi_mr_key(mr.get()) << std::endl;
 
-    fid_pep* pep;
+    fid_pep *pep;
 
-    if(fi_passive_ep(fabric, info, &pep, NULL)){
-        perror("");
-    }
-    if(fi_pep_bind(pep, &eq->eq->fid, 0)){
+    if (fi_passive_ep(fabric.get(), info.get(), &pep, NULL)) {
         perror("");
     }
 
-    if(fi_listen(pep)){
+    if (fi_pep_bind(pep, &eq->fid, 0)) {
         perror("");
     }
 
-    if(fi_close(&pep->fid)){
+    if (fi_listen(pep)) {
         perror("");
     }
 
+    if (fi_close(&pep->fid)) {
+        perror("");
+    }
 
-    delete mr;
-    delete cq;
-    delete domain;
-    delete eq;
     delete[] buf;
-    fi_freeinfo(info);
-
-    if (fi_close(&fabric->fid)) {
-        perror("Close fabric");
-    }
 
     return 0;
 }
