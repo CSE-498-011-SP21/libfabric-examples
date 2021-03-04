3e099e75c599218eefa1fe6ebb8c1dbd4ee10411
jimjbrettj <jdj221@lehigh.edu>
2021-03-04 17:03:21 -0500

    WIP/Connections

--- echo_msg/Echo.cpp
+++ echo_msg/Echo.cpp
@@ -1,13 +1,10 @@
-/*  Notes
-	compile: gcc Echo.cpp -lstdc++ -lfabric
-*/
-
 // standard includes
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <iostream>
 #include <errno.h>
+#include <cassert>
 
 // libfabric includes
 #include <rdma/fabric.h>
@@ -18,23 +15,31 @@
 #include <rdma/fi_rma.h>
 #include <rdma/fi_errno.h>
 
-static char *local_buf;
-static char *remote_buf;
-static struct fi_info *fi_pep, *fi, *hints;
 static struct fid_fabric *fabric;
-static struct fi_eq_attr eq_attr;
-static struct fid_eq *eq;
-static struct fid_pep *pep;
 static struct fid_domain *domain;
-static struct fid_ep *ep;
+static struct fi_info *fi, *fi_pep, *hints;
+static struct fid_eq *eq; // Event queue
+static struct fi_eq_attr eq_attr; // Event queue data
+static struct fid_pep *pep; // Passive endpoint to listen to incoming connection requests
+static struct fid_ep *ep; // Endpoint
 static struct fi_cq_attr cq_attr;
 static struct fid_cq *tx_cq, *rx_cq;
-static struct fid_mr *mr;
-static struct fi_av_attr av_attr;
-static struct fid_av *av;
-static size_t max_msg_size = 4096;
+fi_addr_t remote_addr; // Remote address
 
-static char* dst_addr; // client specifies server
+//Malloc Buffers
+char *local_buf;
+char *remote_buf;
+
+const char *port = "8080";
+int max_msg_size = 4096;
+const char* dst_addr;
+
+/**
+ * Header for RPC
+ */
+struct Header {
+	uint16_t data_len;
+};
 
 // This is pretty neat!
 // From here: https://stackoverflow.com/a/14038590
@@ -46,255 +51,211 @@ inline void callCheck(int err, const char *file, int line, bool abort=true) {
 	}
 }
 
-int check_malloc(char *first, char *second) {
-	int ret = 0;
-	if (!first || !second) {
-        ret = -1;
-    }
-    return ret;
-}
-
-int start_server() {
-	/*
-		Functions in example:
-		- ft_init
-		- ft_init_oob // Dont think I need
-		- ft_getInfo // Since dont have ops (I think) just fi_getinfo()
-		- fi_fabric
-		- fi_eq_open
-		- fi_passive_ep
-		- fi_pep_bind
-		- fi_listen
-    */
-
-	// Init
-    std::cout << "Starting server" << std::endl;
-    hints = fi_allocinfo();
-    hints->caps = FI_MSG;
-    hints->ep_attr->type = FI_EP_MSG;
-
-    // getInfo for server. Assumes server is starting first
-    //safe_call(fi_getinfo(FI_VERSION(1, 6), NULL, "4092", FI_SOURCE, hints, &fi_pep));
-
-
-    if (dst_addr) { // Prob dont need this
-        /* client */
-        safe_call(fi_getinfo(FI_VERSION(1, 6), dst_addr, "4092", 0, hints, &fi_pep));
-    } else {
-        /* server */
-        safe_call(fi_getinfo(FI_VERSION(1, 6), NULL, "4092", FI_SOURCE, hints, &fi_pep));
-    }
-
-    // fi is a linked list of providers. For now just use first one
-    //std::cout << "Using provider: " << fi_pep->fabric_attr->prov_name << std::endl;
-
-     // Create a fabric object. This is the parent of everything else.
-    std::cout << "Creating fabric object" << std::endl;
-    safe_call(fi_fabric(fi_pep->fabric_attr, &fabric, NULL));
-
-    // create and open event queue. Dont think I need to allocate
-    std::cout << "Creating event queue" << std::endl;
-    //memset(&eq_attr, 0, sizeof(eq_attr));
-    //eq_attr.wait_obj = FI_WAIT_UNSPEC;
-    //eq_attr.size = eq_attr.size;
-    safe_call(fi_eq_open(fabric, &eq_attr, &eq, NULL));
-
-    safe_call(fi_passive_ep(fabric, fi_pep, &pep, NULL));
-
-    safe_call(fi_pep_bind(pep, &eq->fid, 0));
-
-    safe_call(fi_listen(pep));
-
-    std::cout << "Done starting server" << std::endl;
-
-    return 0;
+/**
+ * Waits for something to happen in the cq.
+ * @param cq The CQ we are waiting on
+ * @return error
+ */
+int wait_for_completion(struct fid_cq *cq) {
+	std::cout << "Waiting for completion..." << std::endl;
+	struct fi_cq_entry entry = {};
+	int ret;
+	while(true) {
+		ret = fi_cq_read(cq, &entry, 1);
+		if (ret > 0) return 0;
+		if (ret != -FI_EAGAIN) {
+			// New error on queue
+			struct fi_cq_err_entry err_entry = {};
+			fi_cq_readerr(cq, &err_entry, 0);
+			std::cout << fi_strerror(err_entry.err) << " " <<
+						 fi_cq_strerror(cq, err_entry.prov_errno,
+										err_entry.err_data, nullptr, 0) << std::endl;
+			return ret;
+		}
+	}
 }
 
 // I think this okay
 int retrieve_conn_req(struct fid_eq *eq, struct fi_info **fi) {
-	std::cout << "Retrieve Conn request" << std::endl;
+	std::cout << "Retrieving Conn request" << std::endl;
 	struct fi_eq_cm_entry entry;
 	uint32_t event;
 	ssize_t rd;
 	int ret;
 
+	std::cout << "Waiting..." << std::endl;
+
 	// A synchronous (blocking) read of an event queue
 	rd = fi_eq_sread(eq, &event, &entry, sizeof(entry), -1, 0);
 	//rd = fi_eq_read(eq, &event, &entry, sizeof(entry), 0);
-	if (rd != sizeof entry) {
-		return (int) rd;
+	if (ret > 0) {
+		std::cout << "Returned " << rd << std::endl;
+		return 0;
 	}
-	std::cout << entry.info << std::endl;
-
-	*fi = entry.info;
-	if (event != FI_CONNREQ) {
-		ret = -FI_EOTHER;
+	if (ret != -FI_EAGAIN) {
 		return ret;
 	}
 
 	std::cout << "Done Retrieving Conn request" << std::endl;
 
-	return 0;
-}
-
-// I think alloc_active_res is for the queues
-int alloc_ep_res(struct fi_info *fi) {
-	// Create a completion queue. This reports data transfer operation completions.
-    // Unlike fabric event queues, these are associated with a single hardware NIC.
-    // Will create one for tx and one for rx
-    std::cout << "Creating tx completion queue" << std::endl;
-    memset(&cq_attr, 0, sizeof(cq_attr));
-    cq_attr.wait_obj = FI_WAIT_NONE;
-    cq_attr.size = fi->tx_attr->size;
-    safe_call(fi_cq_open(domain, &cq_attr, &tx_cq, NULL));
-
-    std::cout << "Creating rx completion queue" << std::endl;
-    cq_attr.size = fi->rx_attr->size;
-    safe_call(fi_cq_open(domain, &cq_attr, &rx_cq, NULL));
-	return 0;
-}
-
-int alloc_active_res(struct fi_info *fi) {
-	safe_call(alloc_ep_res(fi));
-	safe_call(fi_endpoint(domain, fi, &ep, NULL));
-	return 0;
-
-}
-
-int server_connect(void) {
-	/*
-		Functions in example:
-		- retrieve connection request
-		- fi_domain
-		- fi_domain_bind // maybe
-		- allocate resources
-		- enable_ep_recv // try just fi_enable(ep)
-		- accept connection
-    */
-	std::cout << "Server Connect" << std::endl;
-
-	// Retrieve connection request
-	safe_call(retrieve_conn_req(eq, &fi));
-
-	// Domain
-    std::cout << "Creating domain" << std::endl;
-    safe_call(fi_domain(fabric, fi, &domain, NULL));
-    // maybe need to do fi_domain_bind(domain, &eq->fid, 0)
-
-    // Allocate resources
-    safe_call(alloc_active_res(fi)); // Think this is my tx and rx queue. Also clean up
-
-    safe_call(fi_enable(ep));
-
-    // Accept connection
-	return 0;
-}
-
-// Think okay
-int open_fabric_res(void) {
-	std::cout << "Opening fabric res" << std::endl;
-	safe_call(fi_fabric(fi_pep->fabric_attr, &fabric, NULL));
-	safe_call(fi_eq_open(fabric, &eq_attr, &eq, NULL));
-	safe_call(fi_domain(fabric, fi_pep, &domain, NULL));
-	// Dont think I need to bind domain
-	std::cout << "Done opening fabric res" << std::endl;
-	return 0;
-}
-
-int connect_ep(struct fid_ep *ep, struct fid_eq *eq, fi_addr_t *remote_addr) {
-	safe_call(fi_connect(ep, remote_addr, NULL, 0));
-	return 0;
-}
-
-int client_connect(void) {
-	/*
-		Functions in example:
-		- ft_init
-		- ft_init_oob // Dont think I need
-		- ft_getInfo // Since dont have ops (I think) just fi_getinfo()
-		- ft_open_fabric_res
-		- ft_alloc_active_res
-		- ft_enable_ep_recv
-		- ft_connect_ep
-    */
-
-	// Init
-    std::cout << "Starting client" << std::endl;
-    hints = fi_allocinfo();
-    hints->caps = FI_MSG;
-    hints->ep_attr->type = FI_EP_MSG;
-
-    // getinfo 127.0. 0.1
-    safe_call(fi_getinfo(FI_VERSION(1, 6), dst_addr, "4092", 0, hints, &fi_pep))
-
-    safe_call(open_fabric_res());
-
-    // Allocate resources
-    safe_call(alloc_active_res(fi_pep));
-
-    safe_call(fi_enable(ep));
-
-    std::cout << fi_pep->dest_addr << std::endl;
-
-    safe_call(connect_ep(ep, eq, fi_pep->dest_addr));
-    // int ret = fi_connect(ep, fi_pep->dest_addr, NULL, 0);
-    // if (ret) {
-    // 	std::cout << "Fucking idiot" << std::endl;
-    // }
-
-    std::cout << "Done starting client" << std::endl;
-
-    return 0;
-}
-
-void usage() {
-    std::cout << "Usage: ./Echo [optional server address]" << std::endl;
-    std::cout << "            server address - remote server to connect to as a client." << std::endl;
-    std::cout << "                             If not specified, will run as a server." << std::endl;
+	return -1;
 }
 
 int main(int argc, char **argv) {
-	int ret = 0;
+	hints = fi_allocinfo();
+    hints->caps = FI_MSG;
+    hints->ep_attr->type = FI_EP_MSG;
 
-	// Get command line args
-	if (argc < 2) {
+    // Get command line args
+	if (argc < 2) { // Server
 		std::cout << "Running as SERVER" << std::endl;
-	} else if (argc == 2) {
-		if (argv[1] == "--help") {
-			usage();
-			return 0;
-		} else {
-			dst_addr = argv[1];
-			std::cout <<  "Running as CLIENT - server addr=" << dst_addr << std::endl;
-		}
+		safe_call(fi_getinfo(FI_VERSION(1, 6), NULL, port, FI_SOURCE, hints, &fi));
+	} else if (argc == 2) { // Client
+		dst_addr = argv[1];
+		std::cout <<  "Running as CLIENT - server addr=" << dst_addr << std::endl;
+		safe_call(fi_getinfo(FI_VERSION(1, 6), dst_addr, port, 0, hints, &fi));
 	} else {
 		std::cout << "Too many arguments!" << std::endl;
-        usage();
         return -1;
 	}
 
-	// 1) Init fabric objects
-	//safe_call(init_fabric());
-	if (!dst_addr) {
-		safe_call(start_server());
+	// Create a fabric object.
+    std::cout << "Creating fabric" << std::endl;
+    safe_call(fi_fabric(fi->fabric_attr, &fabric, NULL));
+
+    // Fabric Domain
+    std::cout << "Creating domain" << std::endl;
+	safe_call(fi_domain(fabric, fi, &domain, NULL));
+
+	// Create and open event queue
+    std::cout << "Creating event queue" << std::endl;
+    memset(&eq_attr, 0, sizeof(eq_attr));
+    eq_attr.wait_obj = FI_WAIT_UNSPEC;
+    //eq_attr.size = eq_attr.size; // Might need idk
+    safe_call(fi_eq_open(fabric, &eq_attr, &eq, NULL));
+
+    // Might actually need address vector
+
+    // Think I can just bind eq to the domain
+    //safe_call(fi_domain_bind(domain, &eq->fid, FI_REG_MR));
+
+    // If not then have to make a passive ep then bind it to eq
+    //safe_call(fi_passive_ep(fabric, fi_pep, &pep, nullptr));
+    //safe_call(fi_pep_bind(pep, &eq->fid, 0)); // Might need flags, TBD
+
+    // Create rx and tx completion queues
+    std::cout << "Creating tx completion queue" << std::endl;
+    memset(&cq_attr, 0, sizeof(cq_attr));
+    cq_attr.wait_obj = FI_WAIT_NONE;
+    //cq_attr.format = FI_CQ_FORMAT_CONTEXT;
+    cq_attr.size = fi->tx_attr->size;
+    safe_call(fi_cq_open(domain, &cq_attr, &tx_cq, NULL));
+    std::cout << "Creating rx completion queue" << std::endl;
+    cq_attr.size = fi->rx_attr->size;
+    safe_call(fi_cq_open(domain, &cq_attr, &rx_cq, NULL));
+
+
+    // Endpoint
+    safe_call(fi_endpoint(domain, fi, &ep, NULL))
+
+    // // Bind queues to endpoints
+    std::cout << "Binding TX CQ to EP" << std::endl;
+    safe_call(fi_ep_bind(ep, &tx_cq->fid, FI_TRANSMIT));
+    std::cout << "Binding RX CQ to EP" << std::endl;
+    safe_call(fi_ep_bind(ep, &rx_cq->fid, FI_RECV));
+
+    // Malloc Buffers
+    fid_mr *mr;
+	local_buf = new char[max_msg_size];
+	remote_buf = new char[max_msg_size];
+	memset(remote_buf, 0, max_msg_size);
+
+	safe_call(fi_mr_reg(domain, remote_buf, max_msg_size,
+					FI_SEND | FI_RECV, 0,
+					0, 0, &mr, NULL));
+
+
+    // Enable the endpoint
+ //    std::cout << "Enabling EP" << std::endl;
+	 //safe_call(fi_enable(ep));
+
+	// Specify client vs server behavior
+	if (dst_addr) { // Client
+		std::cout << "Connecting client" << std::endl;
+		// Create endpoint
+		//safe_call(fi_endpoint(domain, fi, &ep, NULL));
+		// Bind to eq
+		safe_call(fi_ep_bind(ep, &eq->fid, 0))
+		safe_call(fi_enable(ep));
+		// Connect
+		safe_call(fi_connect(ep, fi->dest_addr, NULL, 0));
+		safe_call(retrieve_conn_req(eq, &fi));
+
+
+
+
+		// Communication Stuff
+		assert(sizeof(size_t) == sizeof(uint64_t));
+
+		size_t addrlen = 0;
+        fi_getname(&ep->fid, nullptr, &addrlen);
+        char *addr = new char[addrlen];
+        safe_call(fi_getname(&ep->fid, addr, &addrlen));
+
+        //std::cout << "Client: Sending " << addrlen << " " << (void*) addr << " " << fi->dest_addr << std::endl;
+        //safe_call(fi_connect(ep, fi->dest_addr, NULL, 0));
+
+        memcpy(local_buf, &addrlen, sizeof(uint64_t));
+        memcpy(local_buf + sizeof(uint64_t), addr, addrlen);
+
+        std::string data = "Hello, World!";
+		Header header = {};
+		header.data_len = data.length();
+
+		memcpy(local_buf + sizeof(uint64_t) + addrlen, (char *) &header, sizeof(Header));
+		memcpy(local_buf + sizeof(uint64_t) + addrlen + sizeof(Header), data.c_str(), data.length());
+
+		std::cout << "Sending " << data << " to server" << std::endl;
+		safe_call(fi_send(ep, local_buf, sizeof(uint64_t) + addrlen + data.length() + sizeof(Header), NULL, 0, NULL));
+		//safe_call(fi_write(ep, local_buf, sizeof(uint64_t) + addrlen + data.length() + sizeof(Header), nullptr, remote_addr, 0, 0, nullptr));
+		safe_call(wait_for_completion(tx_cq));
+		std::cout << "Client done" << std::endl;
+
+	} else { // Server
+		// Connection stuff
+		std::cout << "Connecting server" << std::endl;
+
+		// Create pep
+		safe_call(fi_passive_ep(fabric, fi, &pep, nullptr));
+
+		// bind pep to eq
+		safe_call(fi_pep_bind(pep, &eq->fid, 0));
+
+		// listen
+		safe_call(fi_listen(pep));
+		//safe_call(wait_for_completion(rx_cq));
+		safe_call(retrieve_conn_req(eq, &fi));
+
+		// After processing, allocate ep
+		//safe_call(fi_endpoint(domain, fi, &ep, NULL));
+		// bind ep to same eq
+		std::cout << "Binding EQ to EP" << std::endl;
+    	safe_call(fi_ep_bind(ep, &eq->fid, 0));
+   
+
+    	safe_call(fi_enable(ep));
+		// accept on ep // make sure its OFI accept not socket accept
+		safe_call(fi_accept(ep, NULL, 0));
+		//safe_call(wait_for_completion(rx_cq));
+
+
+
+		// Communication stuff
+		std::cout << "Server: Receiving client addres" << std::endl;
+		safe_call(fi_recv(ep, remote_buf, max_msg_size, nullptr, 0, nullptr));
+		safe_call(wait_for_completion(rx_cq));
+		std::cout << "Server done" << std::endl;
 	}
-	// 2) Init endpoint
-	//safe_call(init_endpoint());
-	// 3) Bind fabric to endpoint
-	//safe_call(bind_endpoint());
 
-	ret = dst_addr ? client_connect() : server_connect();
-	if (ret) {
-		return ret;
-	}
-
-	if (dst_addr) {
-		// Client stuff
-
-	} else {
-		// Server stuff
-
-	}
-
-	return ret;
+	return 0;
 }
---
+++ echo_msg/Echo2.cpp
@@ -0,0 +1,314 @@
+/*  Notes
+	compile: gcc Echo.cpp -lstdc++ -lfabric
+*/
+
+// standard includes
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <errno.h>
+
+// libfabric includes
+#include <rdma/fabric.h>
+#include <rdma/fi_domain.h>
+#include <rdma/fi_endpoint.h>
+#include <rdma/fi_cm.h>
+#include <rdma/fi_tagged.h>
+#include <rdma/fi_rma.h>
+#include <rdma/fi_errno.h>
+
+static char *local_buf;
+static char *remote_buf;
+static struct fi_info *fi_pep, *fi, *hints;
+static struct fid_fabric *fabric;
+static struct fi_eq_attr eq_attr;
+static struct fid_eq *eq;
+static struct fid_pep *pep;
+static struct fid_domain *domain;
+static struct fid_ep *ep;
+static struct fi_cq_attr cq_attr;
+static struct fid_cq *tx_cq, *rx_cq;
+static struct fid_mr *mr;
+static struct fi_av_attr av_attr;
+static struct fid_av *av;
+const char *port = "8080";
+static size_t max_msg_size = 4096;
+
+static char* dst_addr; // client specifies server
+
+// This is pretty neat!
+// From here: https://stackoverflow.com/a/14038590
+#define safe_call(ans) { callCheck((ans), __FILE__, __LINE__); }
+inline void callCheck(int err, const char *file, int line, bool abort=true) {
+	if (err != 0) {
+		std::cout << "Error: " << err << " " << fi_strerror(-err) << " " << file << ":" << line << std::endl;
+		exit(0);
+	}
+}
+
+int check_malloc(char *first, char *second) {
+	int ret = 0;
+	if (!first || !second) {
+        ret = -1;
+    }
+    return ret;
+}
+
+int start_server() {
+	/*
+		Functions in example:
+		- ft_init
+		- ft_init_oob // Dont think I need
+		- ft_getInfo // Since dont have ops (I think) just fi_getinfo()
+		- fi_fabric
+		- fi_eq_open
+		- fi_passive_ep
+		- fi_pep_bind
+		- fi_listen
+    */
+
+	// Init
+    std::cout << "Starting server" << std::endl;
+    hints = fi_allocinfo();
+    hints->caps = FI_MSG;
+    hints->ep_attr->type = FI_EP_MSG;
+
+
+    if (dst_addr) {
+        /* client */
+        safe_call(fi_getinfo(FI_VERSION(1, 6), dst_addr, port, 0, hints, &fi_pep));
+    } else {
+        /* server */
+        safe_call(fi_getinfo(FI_VERSION(1, 6), NULL, port, FI_SOURCE, hints, &fi_pep));
+    }
+
+    // fi is a linked list of providers. For now just use first one
+    //std::cout << "Using provider: " << fi_pep->fabric_attr->prov_name << std::endl;
+
+     // Create a fabric object. This is the parent of everything else.
+    std::cout << "Creating fabric object" << std::endl;
+    safe_call(fi_fabric(fi_pep->fabric_attr, &fabric, NULL));
+
+    std::cout << "Creating domain" << std::endl;
+    safe_call(fi_domain(fabric, fi_pep, &domain, NULL));
+
+    // create and open event queue. Dont think I need to allocate
+    std::cout << "Creating event queue" << std::endl;
+    memset(&eq_attr, 0, sizeof(eq_attr));
+    eq_attr.wait_obj = FI_WAIT_UNSPEC;
+    eq_attr.size = eq_attr.size;
+    safe_call(fi_eq_open(fabric, &eq_attr, &eq, NULL));
+
+    safe_call(fi_passive_ep(fabric, fi_pep, &pep, NULL));
+
+    std::cout <<"Event queue fid: " << &eq->fid << std::endl;
+    std::cout <<"Passive ep: " << pep << std::endl;
+
+    safe_call(fi_pep_bind(pep, &eq->fid, 0));
+
+    safe_call(fi_listen(pep));
+
+    std::cout << "Done starting server" << std::endl;
+
+    return 0;
+}
+
+// I think this okay
+int retrieve_conn_req(struct fid_eq *eq, struct fi_info **fi) {
+	std::cout << "Retrieving Conn request" << std::endl;
+	struct fi_eq_cm_entry entry;
+	uint32_t event;
+	ssize_t rd;
+	int ret;
+
+	std::cout << "Waiting..." << std::endl;
+
+	// A synchronous (blocking) read of an event queue
+	rd = fi_eq_sread(eq, &event, &entry, sizeof(entry), -1, 0);
+	//rd = fi_eq_read(eq, &event, &entry, sizeof(entry), 0);
+	if (rd != sizeof entry) {
+		return (int) rd;
+	}
+	std::cout << entry.info << std::endl;
+
+	*fi = entry.info;
+	if (event != FI_CONNREQ) {
+		ret = -FI_EOTHER;
+		return ret;
+	}
+
+	std::cout << "Done Retrieving Conn request" << std::endl;
+
+	return 0;
+}
+
+// I think alloc_active_res is for the queues
+int alloc_ep_res(struct fi_info *fi) {
+	// Create a completion queue. This reports data transfer operation completions.
+    // Unlike fabric event queues, these are associated with a single hardware NIC.
+    // Will create one for tx and one for rx
+    std::cout << "Creating tx completion queue" << std::endl;
+    memset(&cq_attr, 0, sizeof(cq_attr));
+    cq_attr.wait_obj = FI_WAIT_NONE;
+    cq_attr.size = fi->tx_attr->size;
+    safe_call(fi_cq_open(domain, &cq_attr, &tx_cq, NULL));
+
+    std::cout << "Creating rx completion queue" << std::endl;
+    cq_attr.size = fi->rx_attr->size;
+    safe_call(fi_cq_open(domain, &cq_attr, &rx_cq, NULL));
+	return 0;
+}
+
+int alloc_active_res(struct fi_info *fi) {
+	safe_call(alloc_ep_res(fi));
+	std::cout << "Domain: " << domain << std::endl;
+	safe_call(fi_endpoint(domain, fi, &ep, NULL));
+	return 0;
+
+}
+
+int server_connect(void) {
+	/*
+		Functions in example:
+		- retrieve connection request
+		- fi_domain
+		- fi_domain_bind // maybe
+		- allocate resources
+		- enable_ep_recv // try just fi_enable(ep)
+		- accept connection
+    */
+	std::cout << "Server Connect" << std::endl;
+
+	// Retrieve connection request
+	safe_call(retrieve_conn_req(eq, &fi));
+
+	// Domain
+    std::cout << "Creating domain" << std::endl;
+    safe_call(fi_domain(fabric, fi, &domain, NULL));
+    fi_domain_bind(domain, &eq->fid, 0);
+
+    // Allocate resources
+    safe_call(alloc_active_res(fi)); // Think this is my tx and rx queue. Also clean up
+
+    safe_call(fi_enable(ep));
+
+    // Accept connection
+	return 0;
+}
+
+// Think okay
+int open_fabric_res(void) {
+	std::cout << "Opening fabric res" << std::endl;
+	safe_call(fi_fabric(fi->fabric_attr, &fabric, NULL));
+	safe_call(fi_eq_open(fabric, &eq_attr, &eq, NULL));
+	safe_call(fi_domain(fabric, fi, &domain, NULL));
+	// Dont think I need to bind domain
+	safe_call(fi_domain_bind(domain, &eq->fid, 0));
+	std::cout << "Done opening fabric res" << std::endl;
+	return 0;
+}
+
+int connect_ep(struct fid_ep *ep, struct fid_eq *eq, fi_addr_t *remote_addr) {
+	safe_call(fi_connect(ep, remote_addr, NULL, 0));
+	return 0;
+}
+
+int client_connect(void) {
+	/*
+		Functions in example:
+		- ft_init
+		- ft_init_oob // Dont think I need
+		- ft_getInfo // Since dont have ops (I think) just fi_getinfo()
+		- ft_open_fabric_res
+		- ft_alloc_active_res
+		- ft_enable_ep_recv
+		- ft_connect_ep
+    */
+
+	// Init
+    std::cout << "Starting client" << std::endl;
+    hints = fi_allocinfo();
+    hints->caps = FI_MSG;
+    hints->ep_attr->type = FI_EP_MSG;
+
+    // getinfo 127.0. 0.1
+    if (!dst_addr) {
+    	std::cout << "No destination address found" << std::endl;
+    }
+    safe_call(fi_getinfo(FI_VERSION(1, 6), dst_addr, port, 0, hints, &fi))
+
+    safe_call(open_fabric_res());
+
+    // Allocate resources
+    safe_call(alloc_active_res(fi));
+
+    //FT_EP_BIND(ep, eq, 0);
+
+    safe_call(fi_enable(ep));
+
+    std::cout << fi->dest_addr << std::endl;
+
+    safe_call(connect_ep(ep, eq, fi->dest_addr));
+    safe_call(fi_connect(ep, remote_addr, NULL, 0));
+    // int ret = fi_connect(ep, fi_pep->dest_addr, NULL, 0);
+    // if (ret) {
+    // 	std::cout << "Fucking idiot" << std::endl;
+    // }
+
+    std::cout << "Done starting client" << std::endl;
+
+    return 0;
+}
+
+void usage() {
+    std::cout << "Usage: ./Echo [optional server address]" << std::endl;
+    std::cout << "            server address - remote server to connect to as a client." << std::endl;
+    std::cout << "                             If not specified, will run as a server." << std::endl;
+}
+
+int main(int argc, char **argv) {
+	int ret = 0;
+
+	// Get command line args
+	if (argc < 2) {
+		std::cout << "Running as SERVER" << std::endl;
+	} else if (argc == 2) {
+		if (argv[1] == "--help") {
+			usage();
+			return 0;
+		} else {
+			dst_addr = argv[1];
+			std::cout <<  "Running as CLIENT - server addr=" << dst_addr << std::endl;
+		}
+	} else {
+		std::cout << "Too many arguments!" << std::endl;
+        usage();
+        return -1;
+	}
+
+	// 1) Init fabric objects
+	//safe_call(init_fabric());
+	if (!dst_addr) {
+		safe_call(start_server());
+	}
+	// 2) Init endpoint
+	//safe_call(init_endpoint());
+	// 3) Bind fabric to endpoint
+	//safe_call(bind_endpoint());
+
+	ret = dst_addr ? client_connect() : server_connect();
+	if (ret) {
+		return ret;
+	}
+
+	if (dst_addr) {
+		// Client stuff
+
+	} else {
+		// Server stuff
+
+	}
+
+	return ret;
+}
